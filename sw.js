32feefcf379648e3ac7a43c1f840091cd5ce858e
Narsi G <narsino@gmail.com>
2026-08-27 11:58:38 -0700

    chore: update checklist

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@
    the hash, which changes these bytes, which is what makes the browser notice
    there is a new worker and install it. Without the stamp the old worker would
    keep serving the old checklist forever. */
-var CACHE = "travel-checklist-b5794c4934";
+var CACHE = "travel-checklist-5b44dd678a";
 var ASSETS = ["./", "./index.html", "./manifest.webmanifest"];
 
 self.addEventListener("install", function(e) {
